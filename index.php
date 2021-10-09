7de14fad105fe7dd063dd2a46f35a66524ee66a3
Marco Andres <ken.andr3s@gmail.com>
2021-10-08 22:08:53 -0400

    proyecto inicial ok

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 require __DIR__ . '/vendor/autoload.php';
 
 echo saludar();
+
+echo saludar_por_nombre('Andres');
--- src/tools.php
+++ src/tools.php
@@ -4,3 +4,8 @@ function saludar()
 {
     return "hola todo el mundo!";
 }
+
+function saludar_por_nombre($nombre)
+{
+    return "Hola " . $nombre . " como estas!";
+}
