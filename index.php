f1ab6d88ad165f790d735bf95062738d801fa1b4
Marco Andres <ken.andr3s@gmail.com>
2021-10-13 20:23:47 -0400

    mejorando codigo

--- index.php
+++ index.php
@@ -12,12 +12,10 @@ echo '<h1>nos vemos luego</hi1>';
 
 echo '<hr>';
 echo '<h1>sumar</h1>';
-echo '<br>';
 
-echo sumar(100, 200);
+echo '<p>' . sumar(100, 200) . '</p>';
 
 echo '<hr>';
 echo '<h1>potencia</h1>';
-echo '<br>';
 
-echo potencia(3,3);
+echo '<p>' . potencia(3,3) . '</p>';
